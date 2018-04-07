c87c0749be6e6ddc62959e47fa3a2c261ba4a29f
Mark <mark@localligence.com>
2018-04-08 03:44:35 +0530

    commit

---
+++ MyAngularApp/ClientApp/app/app.module.shared.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
+import { DashBoardComponent } from './components/dashboard/dashboard.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavBarComponent,
+        DashBoardComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'app', pathMatch: 'full' },
+            //{ path: 'home', component: HomeComponent },
+            //{ path: 'counter', component: CounterComponent },
+            //{ path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'app' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
