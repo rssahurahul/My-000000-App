5df2fa2922c535395bbf47bb2cbb3a3a973fc554
Rahul Sahu <rssahurahul87@gmail.com>
2018-04-08 20:59:32 +0530

    commit

---
+++ MyAngularApp/ClientApp/app/service/config.service.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+export interface config {
+    baseUrl: string,
+    resourceUrl:string
+}
+
+@Injectable()
+export class ConfigService {
+    constructor(private http: HttpClient) {}
+}
+
