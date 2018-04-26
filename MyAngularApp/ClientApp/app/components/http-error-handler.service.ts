bcb5dc4d879adae555c8cd555f4aab9c00965b59
Rahul Sahu <rssahurahul87@gmail.com>
2018-04-26 23:47:55 +0530

    commit

--- MyAngularApp/ClientApp/app/components/http-error-handler.service.ts
+++ MyAngularApp/ClientApp/app/components/http-error-handler.service.ts
@@ -2,6 +2,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of'
 
 import { MessageService } from './message.service';
 
@@ -11,6 +12,15 @@ export type HandleError = <T>(Operation?: string, result?:T) => (error: HttpErro
 export class HttpErrorHandler {
     constructor(private messageService: MessageService) { }
 
-    //createImageBitmap = (ServiceName = '') => <T>
-    //    (operation = 'operation', result = {} as T) => this.handleError=>(serviceName, operation, result);
+    createHandleError = (serviceName = '') => <T>
+        (operation = 'operation', result = {} as T) => this.handleError(serviceName, operation, result);
+
+    handleError<T>(serviceName = '', operation = 'operation', result = {} as T) {
+        return (error: HttpErrorResponse): Observable<T> => {
+            console.error(error);
+            const message = (error.error instanceof ErrorEvent) ? error.message : `server returned code${error.status} with body "${error.error}"`;
+            this.messageService.add(`${serviceName}: ${operation} failed: ${message}`);
+            return of(result);
+        }
+    }
 }
